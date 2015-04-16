4e95d0fad9b46314e20f30f4746b1b0f0cfb9977
yarenty <yarenty@gmail.com>
2015-04-16 14:29:13 +0100

    regina imanges

--- js/custom.js
+++ js/custom.js
@@ -119,7 +119,7 @@ $(document).ready(function(){
 
 // ==========  START GOOGLE MAP ========== //
 function initialize() {
-    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
+    var myLatLng = new google.maps.LatLng(53.435316, -7.935128);
 
     var mapOptions = {
         zoom: 14,
